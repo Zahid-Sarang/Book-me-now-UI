12233e640baca9e3cef31f03eb7112b6852dc63d
zahid-soft <zahidsarang95@gmail.com>
2023-03-06 18:18:12 +0530

    Homepage Lists done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Featured from '../../components/featured/Featured';
 import Header from '../../components/header/Header';
 import Navbar from '../../components/navbar/Navbar';
+import PropertyList from '../../components/propertyList/PropertyList';
 import "./Home.css";
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
       <div className="homeContainer">
         <Featured />
         <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
       </div>
     </div>
   )
