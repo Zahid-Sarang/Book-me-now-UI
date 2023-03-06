d352ae2d19bb31d95e9bfe77952a21ed0bf81f77
zahid-soft <zahidsarang95@gmail.com>
2023-03-06 11:47:52 +0530

    creating routes for the application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-function App() {
+import React from 'react';
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from './pages/home/Home';
+import List from './pages/list/List';
+import Hotel from './pages/hotel/Hotel'
+
+const App = () => {
   return (
-    <div>
-      Hello Guest
-    </div>
-  );
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/hotels' element={<List />} />
+      <Route path='/hotels/:id' element={<Hotel />} />
+    </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/hotel/Hotel.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const hotel = () => {
+  return (
+    <div>hotel</div>
+  )
+}
+
+export default hotel
